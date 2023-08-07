a024acc5dc4668dba95b1c9b04e3c4a96d9f0365
WongYC-66 <yengchien@hotmail.com>
2023-08-07 14:52:44 +0800

    placeShip func of GameBoard

--- factory.js
+++ factory.js
@@ -1,12 +1,71 @@
+const MAX_ROW = 9
+const MAX_COL = 9
+
 export function Ship(length = 0, hit = 0, sunk = false){
     this.length = length
     this.hitCount = hit
     this.sunk = sunk
     this.hit = () => {
         this.hitCount++
+        this.isSunk()
     }
     this.isSunk = () => {
         if(this.length && this.length === this.hitCount) this.sunk = true
         return this.sunk
     }
+    this.isSunk()
+}
+
+export function GameBoard(){
+    this.board = [...Array(10)].map(x => new Array(10).fill(' ')) 
+    this.allShip = []
+    this.missedAttack = []
+    this.correctAttack = []
+    this.placeShip = (coord, dir, size) => {
+        let [rowIdx, colIdx] = coordToBoard(coord)
+        const newShip = new Ship(size, 0, false)
+        if(dir == 'horizontal'){
+            console.log('placing horizontally')
+            if(colIdx + size - 1 > MAX_COL) return console.log('Cannot place here, boundary')
+            let rowCell = this.board[rowIdx].slice(colIdx, colIdx + size)
+            let isEmpty = rowCell.every(cell => cell === ' ')
+            if(! isEmpty) return console.log('Cannot. Other ship is here.')
+            for(let j = colIdx ; j < colIdx + size; j++){
+                this.board[rowIdx][j] = 'o'   // assign ship obj to board
+            }
+        } else if(dir == 'vertical'){
+            console.log('placing vertically')
+            if(rowIdx + size - 1 > MAX_ROW) return console.log('Cannot place here, boundary')
+            let colCell = []
+            for(let i = rowIdx ; i < rowIdx + size; i++){
+                colCell.push(this.board[i][colIdx])
+            }
+            let isEmpty = colCell.every(cell => cell === ' ')
+            if(! isEmpty) return console.log('Cannot. Other ship is here.')
+            for(let i = rowIdx ; i < rowIdx + size ; i++){
+                this.board[i][colIdx] = 'o'   // assign ship obj to board
+            }
+        }
+        this.allShip.push(newShip)
+        
+    }
+    this.receiveAttack = (coord) => {
+        let [x, y] = coordToBoard(coord)
+        let isHit = this.board[x][y] !== ' '
+        if(! isHit){
+            this.missedAttack.push(coord)
+            return
+        } 
+        let foundShip = this.board[x][y]
+        foundShip.hit()
+        this.correctAttack.push(coord)
+    }
+    this.allSunk = () => {
+        return this.allShip.every(ship => ship.sunk)
+    }
+
+}
+
+function coordToBoard(coord){
+    return [coord[1], coord[0]]
 }
--- factory.test.js
+++ factory.test.js
@@ -1,7 +1,71 @@
-import { Ship } from './factory'
+import { Ship, GameBoard } from './factory'
 
-test('create Ship object', () => {
-    const myShip = new Ship(3, 0, false)
-    console.log(myShip)
-    expect(myShip).toEqual({});
+test('create Ship object correctly', () => {
+    const myShip = new Ship(3, 1, false)
+    expect(myShip.sunk).toEqual(false);
+    expect(myShip.length).toEqual(3);
+    expect(myShip.hitCount).toEqual(1);
+    expect(myShip.sunk).toEqual(false);
+    myShip.hit()
+    expect(myShip.hitCount).toEqual(2);
+    expect(myShip.sunk).toEqual(false);
+    myShip.hit()
+    expect(myShip.hitCount).toEqual(3);
+    expect(myShip.sunk).toEqual(true);
+    // check ship to be sunk even argument is false.
+    const sunkShip = new Ship(5, 5, false)
+    expect(sunkShip.length).toEqual(5);
+    expect(sunkShip.hitCount).toEqual(5);
+    expect(sunkShip.sunk).toEqual(true);
 });
+
+test('create gameboard correctly', () => {
+    const myBoard = new GameBoard()
+    expect(myBoard.board).not.toBeNull();
+    expect(myBoard.board.length).toEqual(10);
+    expect(myBoard.board[0].length).toEqual(10);
+    expect(myBoard.board[9].length).toEqual(10);
+    expect(myBoard.board[0][0]).toEqual(' ');
+    expect(myBoard.board[9][9]).toEqual(' ');
+});
+
+test('can place ship', () => {
+    const myBoard = new GameBoard()
+    myBoard.placeShip([0, 0], 'horizontal', 1)
+    expect(myBoard.allShip.length).toEqual(1) 
+    expect(myBoard.board[0][0]).not.toEqual(' ')
+});
+
+test('can place ship at corner if length = 1', () => {
+    const myBoard = new GameBoard()
+    myBoard.placeShip([0, 0], 'horizontal', 1)
+    expect(myBoard.board[0][0]).not.toEqual(' ')
+    myBoard.placeShip([9, 0], 'horizontal', 1)
+    expect(myBoard.board[0][9]).not.toEqual(' ')
+    myBoard.placeShip([0, 9], 'horizontal', 1)
+    expect(myBoard.board[9][0]).not.toEqual(' ')
+    myBoard.placeShip([9, 9], 'horizontal', 1)
+    expect(myBoard.board[9][9]).not.toEqual(' ')
+    expect(myBoard.allShip.length).toEqual(4) 
+});
+
+test('cannot place ship at corner that cross boundary', () => {
+    const myBoard = new GameBoard()
+    myBoard.placeShip([9, 0], 'horizontal', 3)
+    expect(myBoard.allShip.length).toEqual(0) 
+    expect(myBoard.board[0][9]).toEqual(' ')
+    myBoard.placeShip([0, 9], 'vertical', 3)
+    expect(myBoard.allShip.length).toEqual(0) 
+    expect(myBoard.board[9][0]).toEqual(' ')
+});
+
+test('cannot place ship that clashes other', () => {
+    const myBoard = new GameBoard()
+    myBoard.placeShip([4, 4], 'vertical', 5)
+    expect(myBoard.allShip.length).toEqual(1) 
+    myBoard.placeShip([1, 4], 'horizontal', 5)
+    expect(myBoard.allShip.length).toEqual(1) 
+    myBoard.placeShip([4, 2], 'vertical', 5)
+    expect(myBoard.allShip.length).toEqual(1) 
+});
+
